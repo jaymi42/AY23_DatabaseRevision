e3558077f14814087ea7928d8c8eabab4240fce1
jaymi <22036150@myrp.edu.sg>
2023-08-15 12:39:20 +0800

    Added function code to MainActivity and added additional button to activity_main.xml for text view and list view buttons

--- app/src/main/java/com/myapplicationdev/databaserevision/MainActivity.java
+++ app/src/main/java/com/myapplicationdev/databaserevision/MainActivity.java
@@ -2,6 +2,7 @@ package com.myapplicationdev.databaserevision;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -9,27 +10,39 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btnInsertRecord, btnRetrieveRecords;
-//some africans in paris
+    Button btnInsertRecord, btnRetrieveRecordsTv, btnRetrieveRecordsLv;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         btnInsertRecord = findViewById(R.id.btnInsertRecord);
-        btnRetrieveRecords = findViewById(R.id.btnGetRecord);
+        btnRetrieveRecordsTv = findViewById(R.id.btnGetRecordTv);
+        btnRetrieveRecordsLv = findViewById(R.id.btnGetRecordLv);
 
         btnInsertRecord.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, InsertActivity.class);
+                startActivity(intent);
+            }
+        });
 
+        btnRetrieveRecordsTv.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, RetrieveActivityTextView.class);
+                startActivity(intent);
 
             }
         });
 
-        btnRetrieveRecords.setOnClickListener(new View.OnClickListener(){
+        btnRetrieveRecordsLv.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, RetrieveActivityListView.class);
+                startActivity(intent);
 
             }
         });
