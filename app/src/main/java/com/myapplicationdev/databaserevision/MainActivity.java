b75361462b550a7ca1657148e73fd42fc99c314c
jaymi <22036150@myrp.edu.sg>
2023-08-15 12:40:46 +0800

    Added function code to MainActivity and added additional button to activity_main.xml for text view and list view buttons

--- app/src/main/java/com/myapplicationdev/databaserevision/MainActivity.java
+++ app/src/main/java/com/myapplicationdev/databaserevision/MainActivity.java
@@ -35,6 +35,7 @@ public class MainActivity extends AppCompatActivity {
                 Intent intent = new Intent(MainActivity.this, RetrieveActivityTextView.class);
                 startActivity(intent);
 
+
             }
         });
 
