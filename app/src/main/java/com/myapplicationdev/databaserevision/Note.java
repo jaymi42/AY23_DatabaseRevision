57b17127885b7c5ba19946199480a6674ca2cc79
chickennuggetcurrysauce <22001095@myrp.edu.sg>
2023-08-15 12:18:45 +0800

    pojo for note

--- app/src/main/java/com/myapplicationdev/databaserevision/Note.java
+++ app/src/main/java/com/myapplicationdev/databaserevision/Note.java
@@ -1,5 +1,7 @@
 package com.myapplicationdev.databaserevision;
 
+import androidx.annotation.NonNull;
+
 public class Note {
     private int id;
     private String content;
@@ -7,4 +9,43 @@ public class Note {
 
     //TODO: What should be here?
 
+
+    public Note(int id, String content, String priority) {
+        this.id = id;
+        this.content = content;
+        this.priority = priority;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getPriority() {
+        return priority;
+    }
+
+    public void setPriority(String priority) {
+        this.priority = priority;
+    }
+
+    @Override
+    public String toString() {
+        return "Note{" +
+                "id=" + id +
+                ", content='" + content + '\'' +
+                ", priority='" + priority + '\'' +
+                '}';
+    }
 }
